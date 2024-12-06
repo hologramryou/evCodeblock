91c68797e6aeef17887376d60b17ca64a0ae6e63
hologramryou <hologramryou@gmail.com>
2024-12-06 23:39:40 +0700

    Update json.js

--- src/blocks/json.js
+++ src/blocks/json.js
@@ -40,266 +40,392 @@ export const blocks = Blockly.common.createBlockDefinitionsFromJsonArray([{
   "colour": 230,
 },
 {
-  "type": "print",
-  "message0": "%1 %2 %3",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "print"
-    },
-    {
-      "type": "field_label",
-      "name": "COLON",
-      "text": ":"
-    },
-    {
-      "type": "input_value",
-      "name": "MEMBER_VALUE"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 230,
-},
-//Di chuyển
+    "type": "print",
+    "message0": "%1 %2 %3",
+    "args0": [
+      {
+        "type": "field_label",
+        "name": "MEMBER_NAME",
+        "text": "print"
+      },
+      {
+        "type": "field_label",
+        "name": "COLON",
+        "text": ":"
+      },
+      {
+        "type": "input_value",
+        "name": "MEMBER_VALUE"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
+  },
 {
-  "type": "move_backward",
-  "message0": "%1 %2 %3",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Đi lùi"
-    },
-    {
-      "type": "field_label",
-      "name": "COLON",
-      "text": ":"
-    },
-    {
-      "type": "input_value",
-      "name": "MEMBER_VALUE",
-      "check": "StepNumber"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 210,
+    "type": "move",
+    "message0": "%1 %2 %3",
+    "args0": [
+      {
+        "type": "field_label",
+        "name": "MEMBER_NAME",
+        "text": "Di chuyển"
+      },
+      {
+        "type": "field_label",
+        "name": "COLON",
+        "text": ":"
+      },
+      {
+        "type": "input_value",
+        "name": "MEMBER_VALUE"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 230,
 },
 {
-  "type": "move_forward",
-  "message0": "%1 %2 %3",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Tiến lên"
-    },
-    {
-      "type": "field_label",
-      "name": "COLON",
-      "text": ":"
-    },
-    {
-      "type": "input_value",
-      "name": "MEMBER_VALUE",
-      "check": "StepNumber"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 210,
+    "type": "up",
+    "message0": "%1 %2 %3",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Đợi"
+        },
+        {
+            "type": "input_value",
+            "name": "MEMBER_VALUE"
+        },
+        {
+            "type": "field_label",
+            "name": "COLON",
+            "text": "ms"
+        }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "ff9300",
+},
+//my commit
+{
+    "type": "speak",
+    "message0": "%1 %2 %3",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Nói"
+        },
+        {
+            "type": "field_label",
+            "name": "COLON",
+            "text": ":"
+        },
+        {
+            "type": "input_value",
+            "name": "TEXT_TO_SPEAK"
+        }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "f500ff"
 },
 {
-  "type": "turn_left",
-  "message0": "%1 %2 %3",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Quay trái"
-    },
-    {
-      "type": "field_label",
-      "name": "COLON",
-      "text": ":"
-    },
-    {
-      "type": "input_value",
-      "name": "MEMBER_VALUE",
-      "check": "StepNumber"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 210,
+    "type": "think",
+    "message0": "Nghĩ %1",
+    "args0": [
+              {
+                  "type": "input_value",
+                  "name": "TEXT_TO_THINK",
+                  "check": "String"
+              }
+              ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "f500ff",
+    "tooltip": "Stores the specified text to 'think' about",
+    "helpUrl": ""
 },
 {
-  "type": "turn_right",
-  "message0": "%1 %2 %3",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Quay phải"
-    },
-    {
-      "type": "field_label",
-      "name": "COLON",
-      "text": ":"
-    },
-    {
-      "type": "input_value",
-      "name": "MEMBER_VALUE",
-      "check": "StepNumber"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 210,
+    "type": "repeat",
+    "message0": "%1 %2 %3 %4",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Lặp lại"
+        },
+        {
+            "type": "field_label",
+            "name": "COLON",
+            "text": ":"
+        },
+        {
+            "type": "input_value",
+            "name": "MEMBER_VALUE"
+        },
+        {
+            "type": "input_statement",
+            "name": "DO"
+        }
+    ],
+    "colour": "#ff9300",
+    "previousStatement": null,
+    "nextStatement": null
 },
 {
-  "type": "move_backward_infi",
-  "message0": "%1 ",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Đi lùi"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 210,
+    "type": "forever",
+    "message0": "%1 %2 %3",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Liên tục"
+        },
+        {
+            "type": "field_label",
+            "name": "COLON",
+            "text": ":"
+        },
+        {
+            "type": "input_statement",
+            "name": "DO"
+        }
+    ],
+    "colour": "#ff9300",
+    "previousStatement": null,
+    "nextStatement": null
 },
 {
-  "type": "move_forward_infi",
-  "message0": "%1 ",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Tiến lên"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 210,
+    "type": "if",
+    "message0": "%1 %2 %3 %4",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Nếu"
+        },
+        {
+            "type": "input_value",
+            "name": "CONDITION",
+            "check": "Boolean"
+        },
+        {
+            "type": "field_label",
+            "name": "THEN",
+            "text": "thì"
+        },
+        {
+            "type": "input_statement",
+            "name": "DO"
+        }
+    ],
+    "colour": "#ff9300",
+    "previousStatement": null,
+    "nextStatement": null
 },
 {
-  "type": "turn_left_infi",
-  "message0": "%1 ",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Quay trái"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 210,
+    "type": "if_else",
+    "message0": "%1 %2 %3 %4 %5 %6",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Nếu"
+        },
+        {
+            "type": "input_value",
+            "name": "CONDITION",
+            "check": "Boolean"
+        },
+        {
+            "type": "field_label",
+            "name": "THEN",
+            "text": "thì"
+        },
+        {
+            "type": "input_statement",
+            "name": "DO"
+        },
+        {
+            "type": "field_label",
+            "name": "ELSE",
+            "text": "nếu không"
+        },
+        {
+            "type": "input_statement",
+            "name": "DO_ELSE"
+        }
+    ],
+    "colour": "#ff9300",
+    "previousStatement": null,
+    "nextStatement": null
 },
 {
-  "type": "turn_right_infi",
-  "message0": "%1 ",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Quay phải"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 210,
-},
-//Linh kiện 
-{
-  "type": "turn_up",
-  "message0": "%1",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Quay lên 90 độ"
-    },
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 128,
+    "type": "wait_until",
+    "message0": "%1 %2 %3",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Đợi đến khi"
+        },
+        {
+            "type": "input_value",
+            "name": "CONDITION",
+            "check": "Boolean"
+        },
+        {
+            "type": "field_label",
+            "name": "THEN",
+            "text": "thì"
+        }
+    ],
+    "colour": "#ff9300",
+    "previousStatement": null,
+    "nextStatement": null
 },
 {
-  "type": "turn_down",
-  "message0": "%1",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Quay xuống 90 độ"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 180,
+    "type": "repeat_until",
+    "message0": "%1 %2 %3 %4",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Lặp lại cho đến khi"
+        },
+        {
+            "type": "input_value",
+            "name": "CONDITION",
+            "check": "Boolean"
+        },
+        {
+            "type": "field_label",
+            "name": "THEN",
+            "text": "thì"
+        },
+        {
+            "type": "input_statement",
+            "name": "DO"
+        }
+    ],
+    "colour": "#ff9300",
+    "previousStatement": null,
+    "nextStatement": null
 },
 {
-  "type": "rotate",
-  "message0": "%1 %2 %3",
-  "args0": [
-    {
-      "type": "field_label",
-      "name": "MEMBER_NAME",
-      "text": "Quay "
-    },
-    {
-      "type": "field_label",
-      "name": "COLON",
-      "text": ":"
-    },
-    {
-      "type": "input_value",
-      "name": "MEMBER_VALUE",
-      "check": "DegreeNumber"
-    }
-  ],
-  "previousStatement": null,
-  "nextStatement": null,
-  "colour": 180,
+    "type": "stop",
+    "message0": "%1",
+    "args0": [
+              {
+                  "type": "field_label",
+                  "name": "MEMBER_NAME",
+                  "text": "Dừng lại"
+              }
+              ],
+    "colour": "#ff9300",
+    "previousStatement": null,
+    "nextStatement": null
 },
-//Giá trị
+//cảm biến
 {
-  "type": "step",
-  "message0": "%1 bước",
-  "args0": [
-    {
-      "type": "field_number",
-      "name": "NUM",
-      "value": "1",
-      "min": "1",
-      "precision":"1"
-    },
-  ],
-  "output": "StepNumber",
-  "colour": 120,
-  "tooltip": "Số bước ",
+    "type": "key_pressed",
+    "message0": "%1 %2",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "MEMBER_NAME",
+            "text": "Bàn phím: nút đang ấn"
+        },
+        {
+            "type": "field_dropdown",
+            "name": "KEY",
+            "options": [
+                ["A", "a"],
+                ["B", "b"],
+                ["C", "c"],
+                ["D", "d"],
+                ["E", "e"],
+                ["F", "f"],
+                ["G", "g"],
+                ["H", "h"],
+                ["I", "i"],
+                ["J", "j"],
+                ["K", "k"],
+                ["L", "l"],
+                ["M", "m"],
+                ["N", "n"],
+                ["O", "o"],
+                ["P", "p"],
+                ["Q", "q"],
+                ["R", "r"],
+                ["S", "s"],
+                ["T", "t"],
+                ["U", "u"],
+                ["V", "v"],
+                ["W", "w"],
+                ["X", "x"],
+                ["Y", "y"],
+                ["Z", "z"],
+                ["Space", " "],
+                ["Enter", "Enter"]
+            ]
+        }
+    ],
+    "output": "Boolean",
+    "colour":"00e4ff",
+    "tooltip": "Checks if the selected keyboard key is currently pressed",
+    "helpUrl": ""
 },
 {
-  "type": "degree",
-  "message0": "%1 độ",
-  "args0": [
-    {
-      "type": "field_number",
-      "name": "NUM",
-      "value": "0",
-      "min": "0",
-      "max": "180",
-      "precision":"1"
-    },
-  ],
-  "output": "DegreeNumber",
-  "colour": 120,
-  "tooltip": "Góc quay ",
+    "type": "ultrasonic_sensor_status",
+    "message0": "%1 %2",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "SENSOR_NAME",
+            "text": "Cảm biến siêu âm 1:"
+        },
+        {
+            "type": "field_dropdown",
+            "name": "STATUS_TYPE",
+            "options": [
+                ["Khoảng cách", "DISTANCE"],
+                ["Có vật thể", "OBJECT_PRESENT"],
+                ["Không có vật thể", "NO_OBJECT"]
+            ]
+        }
+    ],
+    "output": "Number",
+    "colour": "00e4ff",
+    "tooltip": "Checks the status of Ultrasonic Sensor 1, such as distance or object presence",
+    "helpUrl": ""
+},
+{
+    "type": "ultrasonic_sensor2_status",
+    "message0": "%1 %2",
+    "args0": [
+        {
+            "type": "field_label",
+            "name": "SENSOR_NAME",
+            "text": "Cảm biến siêu âm 2:"
+        },
+        {
+            "type": "field_dropdown",
+            "name": "STATUS_TYPE",
+            "options": [
+                ["Khoảng cách", "DISTANCE"],
+                ["Có vật thể", "OBJECT_PRESENT"],
+                ["Không có vật thể", "NO_OBJECT"]
+            ]
+        }
+    ],
+    "output": "Number",
+    "colour": "00e4ff",
+    "tooltip": "Checks the status of Ultrasonic Sensor 2, such as distance or object presence",
+    "helpUrl": ""
 }
+                                                                          
 ]);
